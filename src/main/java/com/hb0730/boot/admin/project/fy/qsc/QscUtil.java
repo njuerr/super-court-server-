5e96b20c227cd32532d44684ca30de489724edb8
jcq_ljq <1072684103@qq.com>
2022-11-16 11:37:48 +0800

    first

--- src/main/java/com/hb0730/boot/admin/project/fy/qsc/QscUtil.java
+++ src/main/java/com/hb0730/boot/admin/project/fy/qsc/QscUtil.java
@@ -5,6 +5,9 @@ import com.hb0730.boot.admin.project.fy.dto.ListenOpenDTO;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
 /**
  * @program: boot-admin
@@ -82,12 +85,38 @@ public class QscUtil {
         return s;
     }
 
-    public static void getGlobalData() {
-        try {
-           HttpRequestUtil.sendGet("http://localhost:8090/demo5", "");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public static void getGlobalData() throws IOException {
+//        try {
+//           HttpRequestUtil.sendGet("http://localhost:8090/demo5", "");
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+        Socket sc = new Socket("10.0.0.201", 1710);
+        // 为了发送数据，应该获取socket中的输出流
+        OutputStream out = sc.getOutputStream();
+        String s = "{\n" +
+            "    \"jsonrpc\": \"2.0\",\n" +
+            "    \"id\": 1234,\n" +
+            "    \"method\": \"Component.Set\",\n" +
+            "    \"params\": {\n" +
+            "        \"Name\": \"web_remoteMonitor_001\",\n" +
+            "        \"Controls\": [\n" +
+            "            {\n" +
+            "                \"Name\": \"coreIdentify\",\n" +
+            "                \"Value\": 1\n" +
+            "            }\n" +
+            "        ]\n" +
+            "    }\n" +
+            "}\\x00";
+        // write接收字节数据
+        out.write(s.getBytes());
+
+        InputStream is = sc.getInputStream();
+
+        byte[] by = new byte[1024];
+        int len = is.read(by);//read没读到数据会监听等待
+        System.out.println(new String(by, 0 ,len));
+        sc.close();
     }
 
 }
