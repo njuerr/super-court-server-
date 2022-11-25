85fa9bb915c1de102e6194ef2d0b2a66ca443c07
jcq_ljq <1072684103@qq.com>
2022-11-25 10:30:11 +0800

    first

--- src/main/java/com/tingtu/ax/admin/project/system/menu/service/cache/MenuCache.java
+++ src/main/java/com/tingtu/ax/admin/project/system/menu/service/cache/MenuCache.java
@@ -38,6 +38,9 @@ public class MenuCache implements CacheUtil {
     public List<TreeMenuDTO> getMenuCache(String userId) {
         String cacheKey = getCacheKey(keyValue, userId);
         String json = cache.get(cacheKey);
+        if (json==null){
+            json = "[{\"createUserId\":\"-1\",\"createTime\":\"2020-09-08 06:16:13\",\"updateUserId\":\"-1\",\"updateTime\":\"2022-02-23 11:21:34\",\"isEnabled\":1,\"id\":\"1303215545302175746\",\"external\":0,\"iframe\":0,\"cache\":0,\"hidden\":0,\"showParent\":1,\"i18n\":0,\"title\":\"系统监控\",\"enname\":\"monitorManager\",\"parentId\":\"-1\",\"path\":\"/monitor\",\"component\":\"\",\"icon\":\"ep:monitor\",\"sort\":20,\"showLink\":true},{\"createUserId\":\"-1\",\"createTime\":\"2022-11-11 11:00:51\",\"updateUserId\":\"-1\",\"updateTime\":\"2022-11-11 11:04:28\",\"isEnabled\":1,\"description\":\"\",\"id\":\"1590902342799351809\",\"external\":0,\"iframe\":0,\"cache\":0,\"hidden\":0,\"showParent\":1,\"i18n\":0,\"title\":\"法院状态\",\"enname\":\"courtInfo/index.vue\",\"parentId\":\"1303215545302175746\",\"path\":\"/court/info\",\"component\":\"/courtInfo\",\"icon\":\"ep:chat-line-square\",\"sort\":999,\"authority\":[\"court:info:query\"],\"showLink\":true}]";
+        }
         return SimpleJsonProxy.json.fromJson(json, new TypeReference<List<TreeMenuDTO>>() {
         }, objectMapper);
 
